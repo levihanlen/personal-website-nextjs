a063f044bfdd430af89cab47cfcd3a90bd06ee60
Levi Hanlen <134863518+levihanlen@users.noreply.github.com>
2025-10-01 16:57:08 -0700

    refactor: extract InnerBulletList component for better readability

--- app/secret/new/comp.tsx
+++ app/secret/new/comp.tsx
@@ -5,6 +5,44 @@ import { NodeClusterType, UiNodeCluster } from "@/app/utils/knowledge/NEW";
 import { capitalize } from "@/app/utils/utils";
 import { useState } from "react";
 
+function InnerBulletList({
+  item,
+  level = 0,
+}: {
+  item: NodeClusterType;
+  level?: number;
+}) {
+  const [isSummaryFocused, setIsSummaryFocused] = useState(false);
+
+  return (
+    <>
+      <li className={isSummaryFocused ? "blur-sm opacity-50" : ""}>
+        {formatKnowledgeText(item.p)}
+
+        {/* {getAnkiText(item)} */}
+        {item.n &&
+          item.n.map((note, nIdx) => (
+            <ul key={`n-${nIdx}`} className="">
+              <li>
+                <i>{capitalize(note)}</i>
+              </li>
+            </ul>
+          ))}
+        {item.c && <BulletList items={item.c} level={level + 1} />}
+      </li>
+
+      {level === 0 && (
+        <textarea
+          className="w-full lh-input-no-h min-h-[100px]"
+          placeholder="Write your summary here..."
+          onFocus={() => setIsSummaryFocused(true)}
+          onBlur={() => setIsSummaryFocused(false)}
+        />
+      )}
+    </>
+  );
+}
+
 function BulletList({
   items,
   level = 0,
@@ -13,44 +51,29 @@ function BulletList({
   level?: number;
 }) {
   return (
-    <ul>
-      {items.map((item, idx) => {
-        return (
-          <li key={idx}>
-            {formatKnowledgeText(item.p)}
-
-            {/* {getAnkiText(item)} */}
-            {item.n &&
-              item.n.map((note, nIdx) => (
-                <ul key={`n-${nIdx}`} className="">
-                  <li>
-                    <i>{capitalize(note)}</i>
-                  </li>
-                </ul>
-              ))}
-            {item.c && <BulletList items={item.c} level={level + 1} />}
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      <ul>
+        {items.map((item, idx) => {
+          return <InnerBulletList item={item} level={level} key={idx} />;
+        })}
+      </ul>
+    </>
   );
 }
 
 function ParsedNodeCluster({ cluster }: { cluster: UiNodeCluster }) {
-  const [isSummaryFocused, setIsSummaryFocused] = useState(false);
+  // const [isSummaryFocused, setIsSummaryFocused] = useState(false);
 
   return (
     <>
       <h2>{cluster.title ? capitalize(cluster.title) : ""}</h2>
-      <div className={isSummaryFocused ? "blur-sm opacity-50" : ""}>
-        <BulletList items={cluster.nodes} />
-      </div>
-      <textarea
+      <BulletList items={cluster.nodes} />
+      {/* <textarea
         className="w-full lh-input-no-h min-h-[100px]"
         placeholder="Write your summary here..."
         onFocus={() => setIsSummaryFocused(true)}
         onBlur={() => setIsSummaryFocused(false)}
-      />
+      /> */}
     </>
   );
 }
