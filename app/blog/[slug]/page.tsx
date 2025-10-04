8639e1e893a1b780e20dce1e30d11c99af37f28e
Levi Hanlen <134863518+levihanlen@users.noreply.github.com>
2025-10-04 13:52:52 -0700

    refactor: remove commented-out EndCardArticle function and update learning.hls with clearer summarization points

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -69,31 +69,3 @@ export async function generateMetadata({
     description: `Discover top life tips for mastering ${article.frontMatter.title.toLowerCase()}. Improve your life: productivity, health, and wealth.`,
   };
 }
-
-// function EndCardArticle() {
-//   // I copied the below code from page.js of /learn. This is to get the "articles" list
-//   const articleDir = "guides";
-//   const files = fs.readdirSync(path.join(articleDir));
-//   const articles = files.map((filename) => {
-//     const fileContent = fs.readFileSync(
-//       path.join(articleDir, filename),
-//       "utf-8"
-//     );
-//     const { data: frontMatter } = matter(fileContent);
-//     return {
-//       meta: frontMatter,
-//       slug: filename.replace(".mdx", ""),
-//     };
-//   }) as GuideType[];
-//   // code ends here.
-//   return (
-//     <div className="mt-16 flex w-full flex-col items-center px-4 md:px-16">
-//       <div className="flex w-full flex-col items-center  lh-card px-8 sm:px-12 py-16">
-//         <Article className="my-4 text-pretty text-center">
-//           <p className="">Check out the guides on these topics:</p>
-//         </Article>
-//         <RandomGuides articles={articles} />
-//       </div>
-//     </div>
-//   );
-// }
