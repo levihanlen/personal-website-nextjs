dbe9c2446f303ab756b177e93c9f76e2935c48e5
Levi Hanlen <134863518+levihanlen@users.noreply.github.com>
2025-10-04 12:21:05 -0700

    feat: enhance GuideSidebar component with improved slug handling and dynamic chapter display

--- app/secret/new/comp.tsx
+++ app/secret/new/comp.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { RadioCircle } from "@/app/comp/Primitives";
 import { formatKnowledgeText } from "@/app/utils/knowledge/format";
 import { NodeClusterType, UiNodeCluster } from "@/app/utils/knowledge/NEW";
 import { capitalize } from "@/app/utils/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
+import { HiMiniChevronRight } from "react-icons/hi2";
 
 type GuideSidebarData = {
   slug: string;
@@ -112,46 +114,74 @@ function ParsedNodeCluster({ cluster }: { cluster: UiNodeCluster }) {
 
 function GuideSidebar({ guides }: { guides: GuideSidebarData[] }) {
   const pathname = usePathname();
+  const lastPathname = pathname ? pathname.split("/").pop() : "";
 
-  const getCurrentSlug = () => {
-    if (!pathname) return "";
-    const match = pathname.match(/\/secret\/new\/([^/]+)/);
-    return match ? match[1] : "";
-  };
+  const guidePathname = pathname
+    ? pathname.match(/\/secret\/new\/([^/]+)/)?.[1]
+    : "";
 
-  const currentSlug = getCurrentSlug();
+  function slugToTitle(slug: string) {
+    return slug
+      .replace(/-/g, " ")
+      .replace(/\b\w/g, (char) => char.toUpperCase());
+  }
 
   return (
-    <div className="flex flex-col gap-6">
-      <h3 className="text-lg lh-bold text-darkest">Guides</h3>
-      <div className="flex flex-col gap-4">
-        {guides.map((guide) => (
-          <div key={guide.slug} className="flex flex-col gap-2">
-            <Link
-              href={`/secret/new/${guide.slug}`}
-              className={`lh-bold ${
-                guide.slug === currentSlug
-                  ? "text-darkest"
-                  : "text-dark lh-interactive"
-              }`}
-            >
-              {guide.title}
-            </Link>
-            <div className="flex flex-col gap-1 pl-4 border-l-2 border-light">
-              {guide.chapters.map(
-                (chapter: { slug: string; title: string }) => (
-                  <Link
-                    key={chapter.slug}
-                    href={`/secret/new/${guide.slug}/${chapter.slug}`}
-                    className="text-dark lh-interactive"
+    <div className="flex flex-col gap-6 w-full">
+      {/* <h3 className="text-lg lh-bold text-darkest">Guides</h3> */}
+      <div className="flex flex-col text-dark">
+        {guides.map((guide) => {
+          const isCurrentGuide = guide.slug === guidePathname;
+
+          return (
+            <div key={guide.slug}>
+              <Link
+                href={`/secret/new/${guide.slug}`}
+                className="lh-interactive py-1 flex flex-row gap-2 items-center justify-between"
+              >
+                <div className="flex flex-row gap-2 items-center">
+                  <RadioCircle checked={false} />
+                  <span
+                    className={` ${
+                      guide.slug === lastPathname ? "text-darkest lh-bold" : ""
+                    }`}
                   >
-                    {chapter.title}
-                  </Link>
-                )
-              )}
+                    {slugToTitle(guide.slug)}
+                  </span>
+                  <span className="text-sm text-dark lh-card py-0.5 px-2">
+                    {guide.chapters.length}
+                  </span>
+                </div>
+                <HiMiniChevronRight
+                  className={`lh-icon-size shrink-0 transition-transform ${
+                    isCurrentGuide ? "rotate-90" : ""
+                  }`}
+                />
+              </Link>
+              {isCurrentGuide &&
+                guide.chapters.map(
+                  (chapter: { slug: string; title: string }) => (
+                    <Link
+                      key={chapter.slug}
+                      href={`/secret/new/${guide.slug}/${chapter.slug}`}
+                      className="lh-interactive pl-4 py-1 flex flex-row gap-2 items-center"
+                    >
+                      <RadioCircle checked={false} />
+                      <span
+                        className={
+                          chapter.slug === lastPathname
+                            ? "text-darkest lh-bold"
+                            : ""
+                        }
+                      >
+                        {slugToTitle(chapter.slug)}
+                      </span>
+                    </Link>
+                  )
+                )}
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
